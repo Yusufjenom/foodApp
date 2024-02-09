00e0b07b5a6816a186b7b09f024a63c60e3bd3c9
Yusufjenom <jenomy32@gmail.com>
2024-02-10 00:51:08 +0100

    [Backend]build: created the login route for vendors

--- server/routes/vendorRoutes.ts
+++ server/routes/vendorRoutes.ts
@@ -1,6 +1,9 @@
 import express, {Request, Response, NextFunction} from 'express';
+import { vendorLogin } from '../controllers/vendorController';
 
 const vendorRouter = express.Router();
 
+
+vendorRouter.post('/login-vendor', vendorLogin);
 
 export {vendorRouter as vendorRoute};
