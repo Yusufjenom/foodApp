64f583e79af79c2cc112274dcb22c13d23551efa
Yusufjenom <jenomy32@gmail.com>
2024-02-11 02:09:31 +0100

    [Backend]build: created the getvendor cotroller, updatevvendor, and updatevendor services

--- server/controllers/vendorController.ts
+++ server/controllers/vendorController.ts
@@ -14,11 +14,16 @@ export const vendorLogin = CatchErrorFunc(async (req: Request, res: Response) =>
    if(vendor){
      const correctPassword = await ComfirmPassword(password, vendor.password);
      if(correctPassword){
-        const vendorToken = await SignToken(vendor.id);
+        const vendorToken = await SignToken({
+         _id: vendor._id,
+         email: vendor.email,
+         name: vendor.name,
+         foodType: vendor.foodType
+        });
         return res.status(200).json({
             success: true,
             vendorToken,
-            vendor
+            //vendor
         })
      }else{
         throw new HandleError("invalid password")
@@ -26,4 +31,29 @@ export const vendorLogin = CatchErrorFunc(async (req: Request, res: Response) =>
    }else{
     throw new HandleError("invalid email")
    }
+});
+
+export const getVendorProfile = CatchErrorFunc(async (req:Request, res:Response) => {
+    const user = req.user;
+    if(user){
+      const vendor = await VendorModel.findById(user._id)
+      return res.status(200).json({
+         success: true,
+         vendor
+      })
+    }else{
+      throw new HandleError("vendor information not found");
+    }
+});
+
+export const updateVVendorProfile = CatchErrorFunc(async (req:Request, res:Response) => {
+    const {} = req.body;
+    const user = req.user;
+    if(user){
+      const vendor = await VendorModel.findById(user._id)
+    }
+});
+
+export const updateVeendorServvice = CatchErrorFunc(async (req:Request, res:Response) => {
+
 });
