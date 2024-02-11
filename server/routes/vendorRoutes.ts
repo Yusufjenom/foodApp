8c4340c9388e42295d19f126cb2c21aa306b7941
Yusufjenom <jenomy32@gmail.com>
2024-02-11 02:12:51 +0100

    [Backend]build: created routes for the get vendor profile, updatevvendor profile, updatevendorservvice

--- server/routes/vendorRoutes.ts
+++ server/routes/vendorRoutes.ts
@@ -1,9 +1,23 @@
-import express, {Request, Response, NextFunction} from 'express';
-import { vendorLogin } from '../controllers/vendorController';
+import express, { Request, Response, NextFunction } from 'express';
+import {
+    getVendorProfile,
+    updateVVendorProfile,
+    updateVeendorServvice,
+    vendorLogin
+} from '../controllers/vendorController';
+import { Authenticate } from '../middlewares/authMiddlware';
 
 const vendorRouter = express.Router();
 
 
 vendorRouter.post('/login-vendor', vendorLogin);
 
-export {vendorRouter as vendorRoute};
+vendorRouter.use(Authenticate)
+
+vendorRouter.get('/profile', getVendorProfile);
+
+vendorRouter.patch('/profile', updateVVendorProfile);
+
+vendorRouter.patch('/service', updateVeendorServvice);
+
+export { vendorRouter as vendorRoute };
