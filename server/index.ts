73acfa36834105b4c712d30c539561f85d0fbe29
Yusufjenom <jenomy32@gmail.com>
2024-03-16 15:39:25 +0100

    [Backend]refactor: created a startservver function to start thhe entire server

--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,18 @@
 import express from 'express';
+import App from './services/expressApp';
 import dotenv from 'dotenv';
 import {connectToDb} from './database/db';
-import { adminRoute, vendorRoute } from './routes';
-import { ErrorHandler } from './middlewares/ErrorHandler';
-import path from 'path';
+
 
 dotenv.config();
-const app = express();
 const port = 8080;
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'images')));
-app.use(express.json());
-app.use(adminRoute);
-app.use(vendorRoute);
+const startServer = async () => {
+  const app = express();
+  await connectToDb();
+  await App(app);
+  app.listen(port, () => console.log(`server running on port ${port}`));
+};
 
-app.use(ErrorHandler);
-(async function(){
-    try{
-      await connectToDb();
-      app.listen(port, () => console.log(`server running on port ${port}`));
-    }
-    catch(err: any){
-        console.log(err.message)
-    }
-})()
+startServer();
+
