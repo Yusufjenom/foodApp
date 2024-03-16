591d636a530085e703e1107ff1e40f3774ff39a6
Yusufjenom <jenomy32@gmail.com>
2024-03-16 16:08:43 +0100

    [Backend]build: created the clientShoping controller for the client's business logic

---
+++ server/controllers/clientShoppingController.ts
@@ -0,0 +1,25 @@
+import { CatchErrorFunc } from "../utility/CatchErrorFunc";
+import {Request, Response, NextFunction} from 'express';
+
+
+export const getAvailableFood = CatchErrorFunc(async(req: Request, res: Response) => {
+    
+});
+
+
+export const getTopRestaurants = CatchErrorFunc(async (req:Request, res:Response) => {
+
+});
+
+export const getFoodIn30Min = CatchErrorFunc(async (req:Request, res:Response) => {
+    
+});
+
+export const searchForFood = CatchErrorFunc(async (req:Request, res:Response) => {
+
+});
+  
+export const getRestaurantById = CatchErrorFunc(async (req:Request, res:Response) => {
+
+});
+  
