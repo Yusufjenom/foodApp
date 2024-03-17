4d51c9caf1a0fb9013a395ecdda27743a28237a3
Yusufjenom <jenomy32@gmail.com>
2024-03-17 09:44:01 +0100

    [Backend]build: created the userRouter file to accommodate all routes partaining to the user

---
+++ server/routes/userRoutes.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+
+const userRouter = express.Router();
+
+/** signup */
+
+
+/** Login  */
+
+
+/**verify customer account*/
+
+
+/** requeesting OTP*/
+
+
+/**user Profile */
+
+
+/** cart*/
+
+
+/** order */
+
+
+/**paymeent */
+
+
+export {userRouter as userRoute};
