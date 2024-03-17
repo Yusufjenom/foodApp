4ca68195321808ec8f2f384b925251c477837145
Yusufjenom <jenomy32@gmail.com>
2024-03-17 08:24:58 +0100

    [Backend]refactor: updated the getAvailableFood and getTopRestaurant controllers with business logics for each

--- server/controllers/clientShoppingController.ts
+++ server/controllers/clientShoppingController.ts
@@ -1,40 +1,67 @@
 import { VendorModel } from "../models/vendorModel";
 import { CatchErrorFunc } from "../utility/CatchErrorFunc";
-import {Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { HandleError } from "../utility/error";
+import { FoodDoc } from "../models/food";
 
 
-export const getAvailableFood = CatchErrorFunc(async(req: Request, res: Response) => {
+export const getAvailableFood = CatchErrorFunc(async (req: Request, res: Response) => {
     const pincode = req.params.pincode;
-    const result = await VendorModel.find({pincode, serviceAvailable: true})
-    .sort([['rating', 'descending']])
-    .populate("foods")
-     
-    if(result.length > 0){
+    const result = await VendorModel.find({ pincode, serviceAvailable: false })
+        .sort([['rating', 'descending']])
+        .populate("foods")
+
+    if (result.length > 0) {
         res.status(200).json({
             success: true,
             result
         })
-    }else{
+    } else {
         throw new HandleError("Data not found", 404);
     }
-    
-});
-
-
-export const getTopRestaurants = CatchErrorFunc(async (req:Request, res:Response) => {
 
 });
 
-export const getFoodIn30Min = CatchErrorFunc(async (req:Request, res:Response) => {
-    
+
+export const getTopRestaurants = CatchErrorFunc(async (req: Request, res: Response) => {
+    const { pincode } = req.params;
+    const result = await VendorModel.find({ pincode, serviceAvailable: false })
+        .sort([['rating', 'descending']])
+        .limit(1)
+    if (result.length > 0) {
+        res.status(200).json({
+            success: true,
+            result
+        })
+    } else {
+        throw new HandleError("Data not found", 404)
+    }
 });
 
-export const searchForFood = CatchErrorFunc(async (req:Request, res:Response) => {
+export const getFoodIn30Min = CatchErrorFunc(async (req: Request, res: Response) => {
+  const {pincode} = req.params;
+  const result = await VendorModel.find({pincode, serviceAvailable: false})
+  .populate("foods")
+
+  if(result.length > 0){
+    let foodResult: any = [];
+    result.map(vendor => {
+        const foods = vendor.foods as [FoodDoc]
+        foodResult.push(...foods.filter(food => food.readyTime  <= 30));
+    })
+    res.status(200).json({
+        success: true,
+        foodResult
+    })
+  }else{
+    throw new HandleError("Data not found", 404);
+  }
+});
+
+export const searchForFood = CatchErrorFunc(async (req: Request, res: Response) => {
 
 });
-  
-export const getRestaurantById = CatchErrorFunc(async (req:Request, res:Response) => {
+
+export const getRestaurantById = CatchErrorFunc(async (req: Request, res: Response) => {
 
 });
-  
