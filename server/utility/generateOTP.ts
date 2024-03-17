9051313da90c6ef5ebb2c9d66cc4744684440d80
Yusufjenom <jenomy32@gmail.com>
2024-03-17 14:47:04 +0100

    [Backend]refactor: updated the otp algorithm to accommodate the expiry time of the otp

--- server/utility/generateOTP.ts
+++ server/utility/generateOTP.ts
@@ -1,7 +1,9 @@
 
 
-export const genOTP  = () => {
+export const genOTP  = async () => {
     let digit = '0123456789';
-    let newDigit = digit.split('').sort(() => Math.random() - 0.5).join('').slice(-6);
-    return newDigit;
+    let otp = digit.split('').sort(() => Math.random() - 0.5).join('').slice(-6);
+    let expiry = new Date();
+    expiry.setTime(new Date().getTime() + (30 * 60 * 1000))
+    return {otp, expiry};
 };
