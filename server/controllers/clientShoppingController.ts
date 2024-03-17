b2d79455f77e8a7379d19493244ef80029034ece
Yusufjenom <jenomy32@gmail.com>
2024-03-17 07:41:46 +0100

    [Backend]refactor: updated the getAvailablefood controller by a pincode route parameter

--- server/controllers/clientShoppingController.ts
+++ server/controllers/clientShoppingController.ts
@@ -1,13 +1,24 @@
 import { VendorModel } from "../models/vendorModel";
 import { CatchErrorFunc } from "../utility/CatchErrorFunc";
 import {Request, Response, NextFunction} from 'express';
+import { HandleError } from "../utility/error";
 
 
 export const getAvailableFood = CatchErrorFunc(async(req: Request, res: Response) => {
     const pincode = req.params.pincode;
-    const result = (await VendorModel.find({pincode, serviceAvailable: true}))
-    // .sort([["rating", "descending"]])
-    // .populate("foods")
+    const result = await VendorModel.find({pincode, serviceAvailable: true})
+    .sort([['rating', 'descending']])
+    .populate("foods")
+     
+    if(result.length > 0){
+        res.status(200).json({
+            success: true,
+            result
+        })
+    }else{
+        throw new HandleError("Data not found", 404);
+    }
+    
 });
 
 
