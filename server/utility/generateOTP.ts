4917c9b4d868c7fe7e52682305e0af586234fbab
Yusufjenom <jenomy32@gmail.com>
2024-03-17 18:52:14 +0100

    [Backeend]refactor: updateed the generate OTP function by converting the otp string to a number

--- server/utility/generateOTP.ts
+++ server/utility/generateOTP.ts
@@ -2,7 +2,7 @@
 
 export const genOTP  = async () => {
     let digit = '0123456789';
-    let otp = digit.split('').sort(() => Math.random() - 0.5).join('').slice(-6);
+    let otp = Number(digit.split('').sort(() => Math.random() - 0.5).join('').slice(-6));
     let expiry = new Date();
     expiry.setTime(new Date().getTime() + (30 * 60 * 1000))
     return {otp, expiry};
