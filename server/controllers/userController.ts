2aca13d974bd286fe7b16f31c35a4009e7272ee3
Yusufjenom <jenomy32@gmail.com>
2024-03-17 10:00:54 +0100

    [Backend]build: created the functions required for the user to carry out operations

---
+++ server/controllers/userController.ts
@@ -0,0 +1,26 @@
+import express, {Request, Response, NextFunction} from 'express';
+import { CatchErrorFunc } from '../utility/CatchErrorFunc';
+
+export const userSignp = CatchErrorFunc(async (req:Request, res:Response) => {
+
+});
+
+export const userLogin = CatchErrorFunc(async (req:Request, res:Response) => {
+
+});
+
+export const verifyUser = CatchErrorFunc(async (req:Request, res:Response) => {
+
+});
+
+export const requestUserOTP = CatchErrorFunc(async (req:Request, res:Response) => {
+
+});
+
+export const getUserProfile = CatchErrorFunc(async (req:Request, res:Response) => {
+
+});
+
+export const updateUserProfile = CatchErrorFunc(async (req:Request, res:Response) => {
+
+});
