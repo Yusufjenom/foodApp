d0ff54d6fc7ff2a67e6534821162ef68ea5f868e
Yusufjenom <jenomy32@gmail.com>
2024-03-17 10:20:57 +0100

    [Backend]refactor: updated the CreateIUserInput class with some validation properties

--- server/dto/user.dto.ts
+++ server/dto/user.dto.ts
@@ -1,1 +1,15 @@
 import {IsEmail, IsEmpty, Length} from'class-validator';
+
+
+export class CreateUserInputs{
+    @IsEmail()
+    email: string;
+
+    @Length(7, 12)
+    phone: string;
+
+    @Length(6, 12)
+    password: string;
+
+
+};
