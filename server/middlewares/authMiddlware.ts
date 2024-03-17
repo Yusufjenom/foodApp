c1cc7d4f50e0825e9eac232eca3d5c3c5cca603a
Yusufjenom <jenomy32@gmail.com>
2024-03-17 20:16:29 +0100

    [Backend]build: created the authorization middleware fro thhe user authorization

--- server/middlewares/authMiddlware.ts
+++ server/middlewares/authMiddlware.ts
@@ -1,6 +1,6 @@
 import { NextFunction ,Request, Response,} from "express";
 import { AuthPayload } from "../dto/auth.dto";
-import { validateToken } from "../utility/SignToken";
+import { validateToken, validateUserToken } from "../utility/SignToken";
 import { HandleError } from "../utility/error";
 
 
@@ -19,9 +19,15 @@ export const Authenticate = async (req: Request, res:Response, next:NextFunction
      next();
   }else{
     //throw new HandleError("user not authorized");
-    res.status(401).json({
-        success: false,
-        message:"user unauthorised"
-    })
+        throw new HandleError("vendor or admin unauthorized", 401);
   }
 };
+
+export const AuthenticatUser = async (req: Request, res:Response, next:NextFunction) => {
+  const validate = await validateUserToken(req);
+  if(validate){
+    next();
+  }else{
+    throw new HandleError("unauthorized user", 401)
+  }
+}
