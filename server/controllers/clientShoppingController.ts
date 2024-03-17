33886b625487cfe92fe6c06753170ea225699123
Yusufjenom <jenomy32@gmail.com>
2024-03-17 05:00:05 +0100

    [Backend]refactor: updated the getfood contoller

--- server/controllers/clientShoppingController.ts
+++ server/controllers/clientShoppingController.ts
@@ -1,9 +1,13 @@
+import { VendorModel } from "../models/vendorModel";
 import { CatchErrorFunc } from "../utility/CatchErrorFunc";
 import {Request, Response, NextFunction} from 'express';
 
 
 export const getAvailableFood = CatchErrorFunc(async(req: Request, res: Response) => {
-    
+    const pincode = req.params.pincode;
+    const result = (await VendorModel.find({pincode, serviceAvailable: true}))
+    // .sort([["rating", "descending"]])
+    // .populate("foods")
 });
 
 
