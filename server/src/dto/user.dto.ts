1a8658471ac0736cfa1d1b0aa1157306157edc1f
Yusufjenom <jenomy32@gmail.com>
2024-08-22 16:54:27 +0100

    update

--- server/src/dto/user.dto.ts
+++ server/src/dto/user.dto.ts
@@ -1,54 +1,51 @@
-import { IsEmail, IsEmpty, Length } from 'class-validator';
-
+import { IsEmail, IsEmpty, Length } from "class-validator";
 
 export class CreateUserInputs {
-    @IsEmail()
-    email: string;
+  @IsEmail()
+  email: string;
 
-    @Length(7, 12)
-    phone: string;
+  @Length(7, 12)
+  phone: string;
 
-    @Length(6, 12)
-    password: string;
+  @Length(6, 12)
+  password: string;
 
-    firstname: string;
+  firstname: string;
 
-    lastname: string;
+  lastname: string;
 
-    address: string;
-};
+  address: string;
+}
 
 export class UserLoginInputs {
-    @IsEmail()
-    email: string;
+  @IsEmail()
+  email: string;
 
-    @Length(6, 12)
-    password: string;
-
-};
+  @Length(6, 12)
+  password: string;
+}
 
 export interface UserPayload {
-    _id: string;
-    email: string;
-    verified: boolean
-};
+  _id: any;
+  email: string;
+  verified: boolean;
+}
 
 export class EditProfileInput {
-    // @IsEmail()
-    // email: string;
+  // @IsEmail()
+  // email: string;
 
-    // @Length(7, 12)
-    // phone: string;
+  // @Length(7, 12)
+  // phone: string;
 
-    // @Length(6, 12)
-    // password: string;
-    @Length(1, 50)
-    firstname: string;
+  // @Length(6, 12)
+  // password: string;
+  @Length(1, 50)
+  firstname: string;
 
-    @Length(1, 50)
-    lastname: string;
+  @Length(1, 50)
+  lastname: string;
 
-    @Length(1, 50)
-    address: string;
-};
-
+  @Length(1, 50)
+  address: string;
+}
