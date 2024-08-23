f78d4896d1ad5ee2ce60ee36401fcb98c535690b
Yusufjenom <jenomy32@gmail.com>
2024-08-23 16:06:56 +0100

    update

--- server/src/services/expressApp.ts
+++ server/src/services/expressApp.ts
@@ -1,19 +1,22 @@
-import express, { Application } from 'express';
-import { adminRoute, vendorRoute, clientShoppingRoute, userRoute} from '../routes';
-import { ErrorHandler } from '../middlewares/ErrorHandler';
-import path from 'path';
+import express, { Application } from "express";
+import {
+  adminRoute,
+  vendorRoute,
+  clientShoppingRoute,
+  userRoute,
+} from "../routes";
+import { ErrorHandler } from "../middlewares/ErrorHandler";
+import path from "path";
 
 export default async (app: Application) => {
-    app.use(express.urlencoded({extended: true}));
-    app.use(express.static(path.join(__dirname, 'images')));
-    app.use(express.json());
-    app.use(userRoute);
-    app.use(adminRoute);
-    app.use(vendorRoute);
-    app.use(clientShoppingRoute);
-    app.use(ErrorHandler);
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, "images")));
+  app.use(express.json());
+  app.use("/api/v1", userRoute);
+  app.use("/api/v1", adminRoute);
+  app.use("/api/v1", vendorRoute);
+  app.use("/api/v1", clientShoppingRoute);
+  app.use(ErrorHandler);
 
-    return app;
+  return app;
 };
-
-
